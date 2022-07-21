2f4a759093a1b7652d88df4fd0e4efb363def748
AionQRy <anacuse15@hotmail.com>
2022-07-21 16:50:07 +0700

    update

--- pokemon-tailwind/src/App.js
+++ pokemon-tailwind/src/App.js
@@ -1,20 +1,74 @@
-import { Button } from '@mui/material';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+// Import MUI stuff
+import {
+  Card,
+  CardHeader,
+  Switch,
+  CardContent,
+  Box,
+  Container,
+  Typography,
+  FormGroup,
+  FormControlLabel,
+  CssBaseline,
+} from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+// Define theme settings
+const light = {
+  palette: {
+    mode: "light",
+  },
+};
+
+const dark = {
+  palette: {
+    mode: "dark",
+  },
+};
+
+const App = () => {
+  // The light theme is used by default
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  // This function is triggered when the Switch component is toggled
+  const changeTheme = () => {
+    setIsDarkTheme(!isDarkTheme);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1 class="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-
-        <Button variant="text">Text</Button>
-<Button variant="contained">Contained</Button>
-<Button variant="outlined">Outlined</Button>
-      </header>
-    </div>
+    <ThemeProvider theme={isDarkTheme ? createTheme(dark) : createTheme(light)}>
+      <CssBaseline />
+      <Container>
+        <div className="App">
+          <Box component="div" p={5}></Box>
+          <Card>
+            <CardHeader
+              action={
+                <FormGroup>
+                  <FormControlLabel
+                    control={
+                      <Switch checked={isDarkTheme} onChange={changeTheme} />
+                    }
+                    label="Dark Theme"
+                  />
+                </FormGroup>
+              }
+            />
+            <CardContent>
+              <Typography variant="h3" component="h3">
+                Kindacode.com
+              </Typography>
+              <Typography variant="body1">
+                Dark Mode is {isDarkTheme ? "On" : "Off"}
+              </Typography>
+            </CardContent>
+          </Card>
+        </div>
+      </Container>
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
